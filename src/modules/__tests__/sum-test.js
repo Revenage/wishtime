29397d0f7260eca20e2bf2f5b0b87c1fc239ab6d
Revenage <romanvolobuev93@gmail.com>
2016-03-25 15:34:34 +0200

    add Jest and sample of test file

---
+++ src/modules/__tests__/sum-test.js
@@ -0,0 +1,12 @@
+/**
+ * Created by reven on 25.03.2016.
+ */
+
+jest.unmock('../sum'); // unmock to use the actual implementation of sum
+
+describe('sum', () => {
+    it('adds 1 + 2 to equal 3', () => {
+        const sum = require('../sum');
+        expect(sum(1, 2)).toBe(3);
+    });
+});
---
+++ src/modules/sum.js
@@ -0,0 +1,8 @@
+/**
+ * Created by reven on 25.03.2016.
+ */
+
+function sum(a, b) {
+    return a + b;
+}
+module.exports = sum;
